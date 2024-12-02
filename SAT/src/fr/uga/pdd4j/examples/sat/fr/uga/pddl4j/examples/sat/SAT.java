39cdcc53e3f700fb6b3131012db11eb74e0a8c9c
unknown <hellois.bernard@etu.univ-grenoble-alpes.fr>
2024-12-02 13:51:39 +0100

    Squelette du projet

---
+++ SAT/src/fr/uga/pdd4j/examples/sat/fr/uga/pddl4j/examples/sat/SAT.java
@@ -0,0 +1,188 @@
+package fr.uga.pddl4j.examples.sat;
+
+import fr.uga.pddl4j.parser.DefaultParsedProblem;
+import fr.uga.pddl4j.parser.RequireKey;
+import fr.uga.pddl4j.plan.Plan;
+import fr.uga.pddl4j.plan.SequentialPlan;
+import fr.uga.pddl4j.planners.AbstractPlanner;
+import fr.uga.pddl4j.planners.Planner;
+import fr.uga.pddl4j.planners.PlannerConfiguration;
+import fr.uga.pddl4j.planners.ProblemNotSupportedException;
+import fr.uga.pddl4j.planners.SearchStrategy;
+import fr.uga.pddl4j.planners.statespace.search.StateSpaceSearch;
+import fr.uga.pddl4j.problem.DefaultProblem;
+import fr.uga.pddl4j.problem.Problem;
+import fr.uga.pddl4j.problem.State;
+import fr.uga.pddl4j.problem.operator.Action;
+import fr.uga.pddl4j.problem.operator.ConditionalEffect;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import picocli.CommandLine;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ *
+ * @author H. Bernard
+ * @version 1.0 - 02.12.2024
+ */
+@CommandLine.Command(name = "SAT",
+    version = "SAT 1.0",
+    description = "Solves a specified planning problem using A* search strategy.",
+    sortOptions = false,
+    mixinStandardHelpOptions = true,
+    headerHeading = "Usage:%n",
+    synopsisHeading = "%n",
+    descriptionHeading = "%nDescription:%n%n",
+    parameterListHeading = "%nParameters:%n",
+    optionListHeading = "%nOptions:%n")
+public class SAT extends AbstractPlanner {
+
+    /**
+     * The class logger.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(SAT.class.getName());
+
+    /**
+     * Creates a new A* search planner with the default configuration.
+     */
+    public SAT() {
+        this(SAT.getDefaultConfiguration());
+    }
+
+    /**
+     * Creates a new A* search planner with a specified configuration.
+     *
+     * @param configuration the configuration of the planner.
+     */
+    public SAT(final PlannerConfiguration configuration) {
+        super();
+        this.setConfiguration(configuration);
+    }
+
+    /**
+     * Instantiates the planning problem from a parsed problem.
+     *
+     * @param problem the problem to instantiate.
+     * @return the instantiated planning problem or null if the problem cannot be instantiated.
+     */
+    @Override
+    public Problem instantiate(DefaultParsedProblem problem) {
+        final Problem pb = new DefaultProblem(problem);
+        pb.instantiate();
+        return pb;
+    }
+
+    /**
+     * Search a solution plan to a specified domain and problem using A*.
+     *
+     * @param problem the problem to solve.
+     * @return the plan found or null if no plan was found.
+     */
+    @Override
+    public Plan solve(final Problem problem) {
+        // Utiliser la librairie SAT
+    }
+
+    /**
+     * @return <code>true</code> if the configuration is valid <code>false</code> otherwise.
+     */
+    public boolean hasValidConfiguration() {
+        return super.hasValidConfiguration();
+    }
+
+    /**
+     * This method return the default arguments of the planner.
+     *
+     * @return the default arguments of the planner.
+     * @see PlannerConfiguration
+     */
+    public static PlannerConfiguration getDefaultConfiguration() {
+        PlannerConfiguration config = Planner.getDefaultConfiguration();
+        return config;
+    }
+
+    /**
+     * Returns the configuration of the planner.
+     *
+     * @return the configuration of the planner.
+     */
+    @Override
+    public PlannerConfiguration getConfiguration() {
+        final PlannerConfiguration config = super.getConfiguration();
+        return config;
+    }
+
+    /**
+     * Sets the configuration of the planner. If a planner setting is not defined in
+     * the specified configuration, the setting is initialized with its default value.
+     *
+     * @param configuration the configuration to set.
+     */
+    @Override
+    public void setConfiguration(final PlannerConfiguration configuration) {
+        super.setConfiguration(configuration);
+    }
+
+    /**
+     * The main method of the <code>SAT</code> planner.
+     *
+     * @param args the arguments of the command line.
+     */
+    public static void main(String[] args) {
+        try {
+            final SAT planner = new SAT();
+            CommandLine cmd = new CommandLine(planner);
+            cmd.execute(args);
+        } catch (IllegalArgumentException e) {
+            LOGGER.fatal(e.getMessage());
+        }
+    }
+
+    /**
+     * Extracts a search from a specified node.
+     *
+     * @param node    the node.
+     * @param problem the problem.
+     * @return the search extracted from the specified node.
+     */
+    private Plan extractPlan(final Node node, final Problem problem) {
+        Node n = node;
+        final Plan plan = new SequentialPlan();
+        while (n.getAction() != -1) {
+            final Action a = problem.getActions().get(n.getAction());
+            plan.add(0, a);
+            n = n.getParent();
+        }
+        return plan;
+    }
+
+    /**
+     * Returns if a specified problem is supported by the planner. Just ADL problem can be solved by this planner.
+     *
+     * @param problem the problem to test.
+     * @return <code>true</code> if the problem is supported <code>false</code> otherwise.
+     */
+    @Override
+    public boolean isSupported(Problem problem) {
+        return (problem.getRequirements().contains(RequireKey.ACTION_COSTS)
+            || problem.getRequirements().contains(RequireKey.CONSTRAINTS)
+            || problem.getRequirements().contains(RequireKey.CONTINOUS_EFFECTS)
+            || problem.getRequirements().contains(RequireKey.DERIVED_PREDICATES)
+            || problem.getRequirements().contains(RequireKey.DURATIVE_ACTIONS)
+            || problem.getRequirements().contains(RequireKey.DURATION_INEQUALITIES)
+            || problem.getRequirements().contains(RequireKey.FLUENTS)
+            || problem.getRequirements().contains(RequireKey.GOAL_UTILITIES)
+            || problem.getRequirements().contains(RequireKey.METHOD_CONSTRAINTS)
+            || problem.getRequirements().contains(RequireKey.NUMERIC_FLUENTS)
+            || problem.getRequirements().contains(RequireKey.OBJECT_FLUENTS)
+            || problem.getRequirements().contains(RequireKey.PREFERENCES)
+            || problem.getRequirements().contains(RequireKey.TIMED_INITIAL_LITERALS)
+            || problem.getRequirements().contains(RequireKey.HIERARCHY))
+            ? false : true;
+    }
+}
